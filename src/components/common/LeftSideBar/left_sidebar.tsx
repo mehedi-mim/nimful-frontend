8d1056b03fa41bd380b36295ea6551ed8f49c41f
Mehedi Mim <mehedi.hasan@vivasoftltd.com>
2023-06-20 00:40:58 +0600

    who am i and how to connect

--- src/components/common/LeftSideBar/left_sidebar.tsx
+++ src/components/common/LeftSideBar/left_sidebar.tsx
@@ -6,8 +6,8 @@ const LeftSideBar = () => {
 
       <div className="sidebar-header">Webcloud</div>
       <ul className="sidebar-subsections">
-        <li><a href="/webcloud">webcloud</a></li>
-        <li><a href="/webcloud">how to connect?</a></li>
+        <li><a href="/webcloud">Webcloud</a></li>
+        <li><a href="/webcloud">How to connect?</a></li>
       </ul>
       {/* <div className="sidebar-header">Blogs</div>
       <ul className="sidebar-subsections">
@@ -25,9 +25,8 @@ const LeftSideBar = () => {
 
       <div className="sidebar-header">About</div>
       <ul className="sidebar-subsections">
-        <li>nimful</li>
-        <li><a href="/about">who am I?</a></li>
-        <li><a href="/contact">contact me</a></li>
+        <li><a href="/about">Who am I?</a></li>
+        <li><a href="/contact">Contact me</a></li>
       </ul>
     </div>
   );
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,135 +1,57 @@
 import Wrapper from '@/components/common/Wrapper';
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 
 const ProfilePage: FC = () => {
-  const [editing, setEditing] = useState(false); // State to track editing mode
-  const [firstName, setFirstName] = useState('Mehedi ');
-  const [lastName, setLastName] = useState('Mim');
-  const [about, setAbout] = useState("I am bla bla...............................");
-  const [username, setUsername] = useState('torxebec');
-  const [web_seed, setWebSeed] = useState('23423twet242323rwet232323');
-
-  // Function to handle edit mode toggle
-  const toggleEdit = () => {
-    setEditing(!editing);
-  };
-
-  // Function to handle input change
-  const handleInputChange = (event:any, setter:any) => {
-    setter(event.target.value);
-  };
-
   return (
-    <Wrapper hasLeftSidebar={true} hasRightWrapper={false}>
+    <Wrapper hasLeftSidebar={false} hasRightWrapper={false}>
       <div className="profile">
-      <section>
+        <section>
+          <div className="section-header">
+          </div>
           <div className="section-content">
-            <img src="https://scontent.fdac11-1.fna.fbcdn.net/v/t39.30808-6/336923457_221091087242211_1107605500335323964_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEm1uY3TouyFVXLQKSmq24Gw2GlIo4JY5DDYaUijgljkLO0cfA2KXdamqy4Fb-3OPNVP2v_H8uXYYuqLZ5MiGrr&_nc_ohc=BRRd8wCp__YAX82PfIv&_nc_ht=scontent.fdac11-1.fna&oh=00_AfBkTpDoZV3TcEb3EKf_bMvGBUENry4D2qLB8EmvLK9i_w&oe=649460F0" alt="Profile" />
+            <img src="https://avatars.githubusercontent.com/mehedi-mim" alt="Profile" />
           </div>
         </section>
 
-        <section className='seed-section'>
+        <section>
           <div className="section-header">
-            <h2>First Name</h2>
-            <button className="edit-button" onClick={toggleEdit}>
-              edit
-            </button>
+            <h2>Name</h2>
           </div>
           <div className="section-content">
-            {editing ? (
-              <input
-                type="text"
-                value={firstName}
-                onChange={(event) => handleInputChange(event, setFirstName)}
-              />
-            ) : (
-              <p>{firstName}</p>
-            )}
+            <p>Mehedi Hassan Mim</p>
           </div>
         </section>
 
-        <section className='seed-section'>
+        <section>
           <div className="section-header">
-            <h2>Last Name</h2>
-            <button className="edit-button" onClick={toggleEdit}>
-              edit
-            </button>
+            <h2>Username</h2>
           </div>
           <div className="section-content">
-            {editing ? (
-              <input
-                type="text"
-                value={lastName}
-                onChange={(event) => handleInputChange(event, setLastName)}
-              />
-            ) : (
-              <p>{lastName}</p>
-            )}
+            <p>mehedi-mim</p>
           </div>
         </section>
 
-        <section className='seed-section'>
+        <section>
           <div className="section-header">
-            <h2>About</h2>
-            <button className="edit-button" onClick={toggleEdit}>
-              edit
-            </button>
+            <h2>Bio</h2>
           </div>
           <div className="section-content">
-            {editing ? (
-              <input
-                type="number"
-                value={about}
-                onChange={(event) => handleInputChange(event, setAbout)}
-              />
-            ) : (
-              <p>{about}</p>
-            )}
+            <p>Hello, my name is Mim and I work as a software engineer at Vivasoft Limited.</p>
           </div>
         </section>
 
-        <section className='seed-section'>
+        <section>
           <div className="section-header">
-            <h2>Username </h2>
-            <button className="edit-button" onClick={toggleEdit}>
-              edit
-            </button>
+            <h2>Current nim-seed</h2>
           </div>
-          <div className="section-content">
-            {editing ? (
-              <input
-                type="text"
-                value={username}
-                onChange={(event) => handleInputChange(event, setUsername)}
-              />
-            ) : (
-              <p>{username}</p>
-            )}
+          <div className="seed">
+            <p>22cf3262f3f54692ad1b96987463471c</p>
+            <a href="#">generate new?</a>
           </div>
         </section>
-
-        <section className='seed-section'>
-          <div className="section-header">
-            <h2>Your current seed. </h2>
-            <button className="edit-button" onClick={toggleEdit}>
-              new
-            </button>
-          </div>
-          <div className="section-content">
-            {editing ? (
-              <input
-                type="text"
-                value={web_seed}
-                onChange={(event) => handleInputChange(event, setWebSeed)}
-              />
-            ) : (
-              <p>{username}</p>
-            )}
-          </div>
-        </section>
-    </div>
+      </div>
     </Wrapper>
-      );
-    }
-    
+  );
+}
+
 export default ProfilePage;
