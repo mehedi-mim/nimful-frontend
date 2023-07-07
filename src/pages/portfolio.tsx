57891bd344fcfeac8d2a84d9e924438692b53e52
I~am~Mim <torxebec@pop-os.localdomain>
2023-07-07 22:07:27 +0600

    Middle box started

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -6,76 +6,27 @@ const AboutPage: FC = () => {
     <CenterWrapper>
   
   <div className="portfolio-center">
-    <section>
-    <h1>As a software engineer at Vivasoft Limited, my passion lies in learning new technologies. 
-    I possess experience in completing projects from scratch to live deployment, collaborating with teams, 
-    and working across multiple domains. Thanks to my self-assurance, 
-    I adapt quickly to new technologies and excel in communication and teamwork.Want to know more about me? <a className='go' href='http://52.66.247.18:8080/portfolio'>click</a>
-    As a software engineer at Vivasoft Limited, my passion lies in learning new technologies. 
-    I possess experience in completing projects from scratch to live deployment, collaborating with teams, 
-    and working across multiple domains. Thanks to my self-assurance, 
-    I adapt quickly to new technologies and excel in communication and teamwork.Want to know more about me? <a className='go' href='http://52.66.247.18:8080/portfolio'>click</a>
-
-    As a software engineer at Vivasoft Limited, my passion lies in learning new technologies. 
-    I possess experience in completing projects from scratch to live deployment, collaborating with teams, 
-    and working across multiple domains. Thanks to my self-assurance, 
-    I adapt quickly to new technologies and excel in communication and teamwork.Want to know more about me? <a className='go' href='http://52.66.247.18:8080/portfolio'>click</a>
-
-    As a software engineer at Vivasoft Limited, my passion lies in learning new technologies. 
-    I possess experience in completing projects from scratch to live deployment, collaborating with teams, 
-    and working across multiple domains. Thanks to my self-assurance, 
-    I adapt quickly to new technologies and excel in communication and teamwork.Want to know more about me? <a className='go' href='http://52.66.247.18:8080/portfolio'>click</a>
-    As a software engineer at Vivasoft Limited, my passion lies in learning new technologies. 
-    I possess experience in completing projects from scratch to live deployment, collaborating with teams, 
-    and working across multiple domains. Thanks to my self-assurance, 
-    I adapt quickly to new technologies and excel in communication and teamwork.Want to know more about me? <a className='go' href='http://52.66.247.18:8080/portfolio'>click</a>
-    As a software engineer at Vivasoft Limited, my passion lies in learning new technologies. 
-    I possess experience in completing projects from scratch to live deployment, collaborating with teams, 
-    and working across multiple domains. Thanks to my self-assurance, 
-    I adapt quickly to new technologies and excel in communication and teamwork.Want to know more about me? <a className='go' href='http://52.66.247.18:8080/portfolio'>click</a>
-    As a software engineer at Vivasoft Limited, my passion lies in learning new technologies. 
-    I possess experience in completing projects from scratch to live deployment, collaborating with teams, 
-    and working across multiple domains. Thanks to my self-assurance, 
-    I adapt quickly to new technologies and excel in communication and teamwork.Want to know more about me? <a className='go' href='http://52.66.247.18:8080/portfolio'>click</a>
-    As a software engineer at Vivasoft Limited, my passion lies in learning new technologies. 
-    I possess experience in completing projects from scratch to live deployment, collaborating with teams, 
-    and working across multiple domains. Thanks to my self-assurance, 
-    I adapt quickly to new technologies and excel in communication and teamwork.Want to know more about me? <a className='go' href='http://52.66.247.18:8080/portfolio'>click</a>
-    As a software engineer at Vivasoft Limited, my passion lies in learning new technologies. 
-    I possess experience in completing projects from scratch to live deployment, collaborating with teams, 
-    and working across multiple domains. Thanks to my self-assurance, 
-    I adapt quickly to new technologies and excel in communication and teamwork.Want to know more about me? <a className='go' href='http://52.66.247.18:8080/portfolio'>click</a>
-    As a software engineer at Vivasoft Limited, my passion lies in learning new technologies. 
-    I possess experience in completing projects from scratch to live deployment, collaborating with teams, 
-    and working across multiple domains. Thanks to my self-assurance, 
-    I adapt quickly to new technologies and excel in communication and teamwork.Want to know more about me? <a className='go' href='http://52.66.247.18:8080/portfolio'>click</a>
-    As a software engineer at Vivasoft Limited, my passion lies in learning new technologies. 
-    I possess experience in completing projects from scratch to live deployment, collaborating with teams, 
-    and working across multiple domains. Thanks to my self-assurance, 
-    I adapt quickly to new technologies and excel in communication and teamwork.Want to know more about me? <a className='go' href='http://52.66.247.18:8080/portfolio'>click</a>
-    As a software engineer at Vivasoft Limited, my passion lies in learning new technologies. 
-    I possess experience in completing projects from scratch to live deployment, collaborating with teams, 
-    and working across multiple domains. Thanks to my self-assurance, 
-    I adapt quickly to new technologies and excel in communication and teamwork.Want to know more about me? <a className='go' href='http://52.66.247.18:8080/portfolio'>click</a>
-    As a software engineer at Vivasoft Limited, my passion lies in learning new technologies. 
-    I possess experience in completing projects from scratch to live deployment, collaborating with teams, 
-    and working across multiple domains. Thanks to my self-assurance, 
-    I adapt quickly to new technologies and excel in communication and teamwork.Want to know more about me? <a className='go' href='http://52.66.247.18:8080/portfolio'>click</a>
-    </h1>
+    <section className='portfolio-top'>
+      <div className='portfolio-top-left'>
+            <h1>Hi.....</h1>
+      </div>
+      <div className='portfolio-top-middle'>
+            <h1>Hi.....</h1>
+      </div>
+      <div className='portfolio-top-right'>
+            <h1>Hi.....</h1>
+      </div>
     </section>
 
-    <section>
+    <section className='portfolio-objective'>
     <h1>As a software engineer at Vivasoft Limited, my passion lies in learning new technologies. 
     I possess experience in completing projects from scratch to live deployment, collaborating with teams, 
     and working across multiple domains. Thanks to my self-assurance, 
     I adapt quickly to new technologies and excel in communication and teamwork.Want to know more about me? <a className='go' href='http://52.66.247.18:8080/portfolio'>click</a></h1>
     </section>
 
-    <section>
-    <h1>As a software engineer at Vivasoft Limited, my passion lies in learning new technologies. 
-    I possess experience in completing projects from scratch to live deployment, collaborating with teams, 
-    and working across multiple domains. Thanks to my self-assurance, 
-    I adapt quickly to new technologies and excel in communication and teamwork.Want to know more about me? <a className='go' href='http://52.66.247.18:8080/portfolio'>click</a></h1>
+    <section className='portfolio-skills'>
+       
     </section>
 
     <section>
