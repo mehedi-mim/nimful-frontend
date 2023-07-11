8cba09aca53fdc26228602f366f3c33d95caf8ca
I~am~Mim <torxebec@pop-os.localdomain>
2023-07-12 00:18:00 +0600

    Portfolio page done

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,17 +10,17 @@ const AboutPage: FC = () => {
           <img src="/images/about/about.png" alt="Profile" className="profile-image" />
         </div>
         <div className="centered-container">
-          <h2>Nimful is a self-productivity website designed to enhance users' efficiency and focus.
-            The main functionality of Nimful revolves around interacting with browser extensions.
-            One of its key features is the ability to capture user activity whenever they switch tabs.
-            Nimful will provide a comprehensive tutorial section, offering insights into the website's backend and frontend workings.
+          <h2>Nimful is an impressive portfolio endeavor that incorporates a captivating wordcloud feature. 
+            Moreover, it boasts a Chrome extension that seamlessly interacts with the backend, meticulously
+             tracking users' visits to diverse websites. The extension diligently logs the duration of each visit and frequency,
+              providing insightful data. 
           </h2>
           <h2>
-          Users can learn about the technical aspects of Nimful, gaining knowledge in various areas.The tutorial section serves as a valuable resource for those interested in understanding the inner workings of the website.
-            Nimful focuses on improving productivity by helping users stay organized and manage their time effectively.
-            Through its extension integration, Nimful offers seamless synchronization and data tracking across different tabs.
-            The website encourages users to adopt efficient work habits by providing tools and insights to optimize their workflow.
-            Nimful's user-friendly interface and informative tutorials make it a valuable resource for individuals striving to boost their productivity.
+          Looking ahead, Nimful envisions an upcoming tutorial section that delves deep into the project's inner workings.
+           This comprehensive guide will shed light on the intricate collaboration among the frontend, backend, and extension components, 
+           unveiling the secret to a flawless user experience. Aspiring junior and mid-level engineers will find this resource invaluable, 
+           gaining profound insights and practical
+           guidance on constructing analogous projects while gaining a deeper understanding of the underlying technologies involved.
           </h2>
         </div>
 
--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -60,12 +60,12 @@ const AboutPage: FC = () => {
           <div className='portfolio-top-left balloon-animation-left'>
             <ul>
               <li> 💼 2.5+ years work-experience.</li>
-              <li> 💼 Backend+(Basic Frontend).</li>
-              <li> 💼 Python Developer.</li>
-              <li> 💼 4 Live Projects.</li>
-              <li> 💼 REST,GraphQL.</li>
-              <li> 💼 Dockerized multiple projects.</li>
-              <li> 💼 Languages( Bangla, English, Hindi).</li>
+              <li> ⚡ Backend+(Basic Frontend).</li>
+              <li> 💻 Python Developer.</li>
+              <li> 🏆 4 Live Projects.</li>
+              <li> 🧩 REST,GraphQL.</li>
+              <li> 🔮 Dockerized multiple projects.</li>
+              <li> 🌐 Languages( Bangla, English, Hindi).</li>
 
             </ul>
           </div>
@@ -84,11 +84,11 @@ const AboutPage: FC = () => {
           </div>
           <div className='portfolio-top-right balloon-animation-right'>
             <ul>
-              <li>💪 Competetive Programmer. </li>
-              <li>📝 Datastructure & Algorithms. </li>
+              <li>🧠 Competetive Programmer. </li>
+              <li>📊 Datastructure & Algorithms. </li>
               <li>🏆 2000+ problems solved.  </li>
               <li>🎓 ACM ICPC participation.  </li>
-              <li>🏆 Yellow coder at toph.co  </li>
+              <li>🎯 Yellow coder at toph.co  </li>
               <li>🤝 Team player.</li>
             </ul>
           </div>
@@ -101,11 +101,10 @@ const AboutPage: FC = () => {
               I extend my warmest greetings as I introduce myself, Mehedi Hasan Mim, an accomplished Software Engineer currently affiliated with the distinguished organization, Vivasoft Limited. With an unwavering dedication to the pursuit of excellence, I have diligently honed my expertise in the art of website development over the course of the past 2.5 years. Prior to this, I had the privilege of immersing myself in the intricacies of mobile application development, and I have now directed my focus towards the dynamic realm of backend software engineering, specializing in the utilization of the esteemed Python programming language.
             </p>
             <p>
-              My journey as a pivotal member of the Vivasoft team has been defined by a deep-seated responsibility for spearheading critical tasks associated with a diverse range of client projects. As a seasoned professional, I bring forth a wealth of expertise in conducting meticulous requirement analysis, crafting intricate system designs, seamlessly integrating new release features. Leveraging my experiences across various domains, I have diligently cultivated a versatile skill set, enabling me to adroitly navigate through complex challenges and intricacies that often arise during the development lifecycle. 
-              My unwavering dedication allows me to deftly identify potential pitfalls and swiftly devise optimal solutions, ensuring the seamless and timely delivery of projects while maintaining an unwavering commitment to quality and client satisfaction.
+              My journey as a pivotal member of the Vivasoft team has been defined by a deep-seated responsibility for spearheading critical tasks associated with a diverse range of client projects. As a seasoned professional, I bring forth a wealth of expertise in conducting meticulous requirement analysis, crafting intricate system designs, seamlessly integrating new release features. Leveraging my experiences across various domains, I have diligently cultivated a versatile skill set, enabling me to adroitly navigate through complex challenges and intricacies that often arise during the development lifecycle.
             </p>
             <p>
-            With an acute understanding that every project presents its unique set of complexities, I approach each endeavor with a meticulous attention to detail, ensuring that I adapt my expertise to cater to the specific requirements and intricacies of each task. My unwavering focus on staying abreast of the latest industry trends and technological advancements empowers me to leverage cutting-edge tools and methodologies, thereby facilitating the development of highly scalable, efficient, and secure software solutions.
+              With an acute understanding that every project presents its unique set of complexities, I approach each endeavor with a meticulous attention to detail, ensuring that I adapt my expertise to cater to the specific requirements and intricacies of each task. My unwavering focus on staying abreast of the latest industry trends and technological advancements empowers me to leverage cutting-edge tools and methodologies, thereby facilitating the development of highly scalable, efficient, and secure software solutions.
             </p>
           </h1>
         </section>
@@ -199,12 +198,37 @@ const AboutPage: FC = () => {
               </div>
               <div className="exp-describe">
                 <ul>
-                  <li>💪  Developed a web‐based product with user management, scraping products from URLS, server‐side notifications,
-                    and the ability to capture live data (video, comments, likes, followers) from live‐streaming platforms.</li>
-                  <li>💪  Implemented multiple web application features such as invoices, payment system, certificate generation per donation,
-                    and report generation from customer surveys via Questionnaire.</li>
-                  <li>💪  Designed and implemented a live chatting system where customers can directly communicate with internal users.</li>
-                  <li>💪  Dockerized multiple project’s backends and database services which helped to deploy rapidly and gave better portability.</li>
+                  <li>
+                  ● Developed a web‐based product with user management, scraping products from URLS, server‐side notifications,
+                    and the ability to capture live data (video, comments, likes, followers) from live‐streaming platforms.
+                  </li>
+
+                  <li>
+                  ●  Implemented multiple web application features such as invoices, payment system, certificate generation per donation,
+                    and report generation from customer surveys via Questionnaire.
+                  </li>
+
+                  <li>
+                  ● Maintain effective one-to-one and team communication channels, including email, video calls, and messaging,
+                    for requirements analysis and bottleneck resolution in order to ensure clear understanding and collaborative problem-solving.
+                  </li>
+
+                  <li>
+                  ●  Designed and implemented a live chatting system where customers can directly communicate with internal users.
+                  </li>
+
+                  <li>
+                  ● Implemented Stripe for secure payment processing, leveraging its robust security features and compliance with industry
+                    standards to ensure the protection of sensitive customer information.
+                  </li>
+
+                  <li>
+                  ●  Dockerized multiple project’s backends and database services which helped to deploy rapidly and gave better portability.
+                  </li>
+
+
+
+
                 </ul>
               </div>
               {/* <div className='exp-skills'>
@@ -222,10 +246,10 @@ const AboutPage: FC = () => {
               </div>
               <div className="exp-describe">
                 <ul>
-                  <li>💪 Implemented features of a mobile application such as the ability for users to upload images
+                  <li>● Implemented features of a mobile application such as the ability for users to upload images
                     via camera and gallery from various types of mobile devices with editing options, and image size was reduced to ensure image quality.</li>
-                  <li>💪 Fixed bugs in that mobile application for leave policy, different UI interfaces, scrolling view, and downloading pdf/image files.</li>
-                  <li>💪 Reduced time and memory complexity performance of a feature mobile punch by 10% where geo‐location attendance for employees
+                  <li>● Fixed bugs in that mobile application for leave policy, different UI interfaces, scrolling view, and downloading pdf/image files.</li>
+                  <li>● Reduced time and memory complexity performance of a feature mobile punch by 10% where geo‐location attendance for employees
                     was taken from the application in real‐time whereabouts, replacing some heavy recursive code functionality.</li>
                 </ul>
               </div>
@@ -247,28 +271,35 @@ const AboutPage: FC = () => {
             <div className='projects-grid-item'>
               <img src="images/portfolio/projects/stickler.jpeg" alt="Stickler"></img>
               <h1>Stickler</h1>
-              <h6>October 2021- Present</h6>
-              <h2>We believe that Live Commerce will be the ascendant ecommerce trend of the coming years.
-                Following on from 6 huge years of growth in China, where Live Commerce went from inception in 2016 to around a third of all ecommerce transactions.
-                Outside China, this will look different, more fragmented, more specialised, and even more interactive.
-                It will be one of the dominant forms of communication and sales for brands and retailers.</h2>
-              <h3>FastAPI,GraphQL,PostgresQL</h3>
+              <h6>October 2022- Present</h6>
+              <h2>Stickler is an advanced analytical platform designed to empower streamers and retailers with comprehensive tools to analyze
+                livestream data and make data-driven decisions.It offers a seamless experience for streamers and retailers to enhance their livestreams and maximize their potential.
+                The Livestream module enables streamers to effortlessly capture and broadcast their livestreams in real-time.
+                Livescope module offers streamers the ability to track and analyze data from other streamers,
+                enabling them to make informed decisions and optimize their own livestreaming strategies.
+                They can subscribe to Stickler to gain access to its comprehensive platform and utilize its features.
+                The subscription model allows streamers to unlock the full potential of Stickler and maximize their livestreaming capabilities.
+              </h2>
+              <h3>Python(FastAPI), GraphQL(strawberry), PostgresQL, Redis, S3, Fargate</h3>
             </div>
             <div className='projects-grid-item'>
-              <img src='images/portfolio/projects/moonraker.png'></img>
+              <img src='images/portfolio/projects/final_moon.png'></img>
               <h1>Toja connected water</h1>
               <h6>January 2022 - September-2022</h6>
-              <h2>Our passion is water, because water is the planet’s most precious resource.
-                Our goal is to balance your water footprint by giving fresh and clean water back to the ecosystem so that we, and generations after us, can still enjoy water.
-                Toja connects you with water projects around the world to work together to raise water awareness,
-                combat water scarcity and provide access to fresh and clean water in remote and poor areas.
-                Your water offset makes it possible. Make your contribution to the environment and society with Toja.
+              <h2>In this innovative system, an extensive network of IoT devices is deployed to track the water consumption of various organizations.
+                These IoT devices are strategically installed to accurately measure and monitor water usage in real-time.
+                Each device collects and transmits water usage data to a centralized platform, ensuring a comprehensive and up-to-date overview of consumption patterns.
+                The system calculates the amount of water consumed by each organization, and based on predetermined compensation or donation rates,
+                it automatically calculates the corresponding monetary value.Companies have
+                the option to compensate for their water consumption by subscribing to a compensation plan.To facilitate seamless transactions and secure payments,
+                the system integrates with Stripe, a trusted and widely used payment gateway.With approximately 96,000 data points captured per day,
+                the system generates real-time statistics and updates the relevant metrics daily.
               </h2>
-              <h3>FastAPI,Firebase, RestAPI,React</h3>
+              <h3>FastAPI, Firebase, PostgreSQL, S3, Stripe</h3>
             </div>
 
             <div className='projects-grid-item'>
-              <img src='images/portfolio/projects/tipsoi.webp'></img>
+              <img src='images/portfolio/projects/final_tip.png'></img>
               <h1>Tipsoi Mobile App</h1>
               <h6>January 2021 - July-2022</h6>
               <h2>Tipsoi HRM comes with a feature-rich & robust android and iOS native mobile app,
@@ -280,38 +311,38 @@ const AboutPage: FC = () => {
                 Easy Activation through QR-code Scan
                 Easy Enrollment and Allocation
               </h2>
-              <h3>Dart,Flutter</h3>
-            </div>
-
-            <div className='projects-grid-item'>
-              <img src='images/portfolio/projects/its.png'></img>
-              <h1>Internal Ticketing System</h1>
-              <h6>July 2021 - December-2022</h6>
-              <h2>Tipsoi HRM comes with a feature-rich & robust android and iOS native mobile app,
-                from where the admin can have the idea of the overall organization’s attendance at-a-glance,and execute various HR functionalities i.e.
-                approve decline leave, manual entry requests. Employees can easily perform their day-to-day activities.
-                Mobile Punch
-                Site Management
-                Apply & Approve Leave/manual entry Requests
-                Easy Activation through QR-code Scan
-                Easy Enrollment and Allocation
-              </h2>
-              <h3>Dart,Flutter</h3>
+              <h3>Dart, Flutter, Firebase</h3>
             </div>
             <div className='projects-grid-item'>
-              <img src='images/portfolio/projects/nimful.png'></img>
+              <img src='images/portfolio/projects/final_nimful.png'></img>
               <h1>Nimful</h1>
               <h6>March 2023 - Present</h6>
-              <h2>Tipsoi HRM comes with a feature-rich & robust android and iOS native mobile app,
-                from where the admin can have the idea of the overall organization’s attendance at-a-glance,and execute various HR functionalities i.e.
-                approve decline leave, manual entry requests. Employees can easily perform their day-to-day activities.
-                Mobile Punch
-                Site Management
-                Apply & Approve Leave/manual entry Requests
-                Easy Activation through QR-code Scan
-                Easy Enrollment and Allocation
+              <h2>
+                Nimful is an impressive portfolio project that incorporates an exciting feature called wordcloud.
+                Additionally, it includes a Chrome extension that interacts with the backend to track users' visits to various websites.
+                The extension records the amount of time a user spends on each site and how many times they visit it.
+
+                In the future, Nimful will introduce a tutorial section that delves into the inner workings of the project.
+                This tutorial will provide a comprehensive overview of how the frontend, backend, and extension components collaborate
+                to create a seamless user experience. This section will be particularly helpful for junior and mid-level engineers,
+                as it will offer valuable insights and guidance on building similar projects and understanding the technologies involved.
               </h2>
-              <h3>Dart,Flutter</h3>
+              <h3>FastAPI, PostgreSQL, React</h3>
+            </div>
+            <div className='projects-grid-item'>
+              <img src='images/portfolio/projects/final_its.png'></img>
+              <h1>Internal Ticketing System</h1>
+              <h6>July 2021 - December-2022</h6>
+              <h2>A SaaS application efficiently handles multiple organizational interfaces by allowing customers to create tickets based on Department, Service Category, and Priority.
+                Assigned users resolve these tickets, ensuring timely resolution.
+                When a customer creates a ticket, they can specify the relevant Department to ensure that it reaches the appropriate
+                team or individual responsible for handling that particular area.
+                 This feature helps to ensure that the right experts are assigned to address the specific issues or inquiries raised by the customers.
+                Additionally, this application integrates a live chatting system.
+                This feature enables customers to have real-time conversations with support agents or assigned users.
+                It provides a convenient and interactive platform for customers to receive immediate assistance  and receive updates on their tickets.
+              </h2>
+              <h3>(FastAPI,Django) , MySQL, PostgreSQL, Websocket</h3>
             </div>
           </div>
 
